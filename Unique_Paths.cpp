eaf2fc0c739728121aea8341258e4f0de91a4365
Shaik Safiullah Sahil Hussain <sahil24072005@gmail.com>
2025-08-28 12:00:00 +0530

    Solved: Unique Paths

---
+++ Unique_Paths.cpp
@@ -0,0 +1,32 @@
+/*
+ * PROBLEM: Unique Paths
+ * PLATFORM: LeetCode
+ * DIFFICULTY: Medium
+ * LEETCODE URL: https://leetcode.com/problems/unique-paths/
+ * * PROBLEM DESCRIPTION:
+ * There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+ * * APPROACH:
+ * Solved using Math to optimize the approach.
+ * Topics: Math, Dynamic Programming, Combinatorics
+ */
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,1));
+        for(int i=2;i<=m;i++){
+            for(int j=2;j<=n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m][n];
+
+    }
+};
+
+// Time Complexity: O(N)
+// Space Complexity: O(N^2)
