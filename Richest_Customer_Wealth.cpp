99de2a50f831a9a2c738afa77664095fe557ee65
Shaik Safiullah Sahil Hussain <sahil24072005@gmail.com>
2025-11-05 13:00:00 +0530

    Solved: Richest Customer Wealth

---
+++ Richest_Customer_Wealth.cpp
@@ -0,0 +1,30 @@
+/*
+ * PROBLEM: Richest Customer Wealth
+ * PLATFORM: LeetCode
+ * DIFFICULTY: Easy
+ * LEETCODE URL: https://leetcode.com/problems/richest-customer-wealth/
+ * * PROBLEM DESCRIPTION:
+ * You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+ * * APPROACH:
+ * Solved using Array to optimize the approach.
+ * Topics: Array, Matrix
+ */
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxi = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].size(); j++) 
+                sum += accounts[i][j];
+            maxi = max(maxi, sum);
+        }
+        return maxi;
+    }
+};
+
+// Time Complexity: O(N)
+// Space Complexity: O(N^2)
